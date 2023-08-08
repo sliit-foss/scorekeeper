37be7d2aae171e207de4271903106e12c39764b5
Akalanka <akalankaperera128@gmail.com>
2023-08-09 01:44:22 +0530

    Fix: added api key to headers

--- src/services/score.js
+++ src/services/score.js
@@ -2,7 +2,10 @@ const serviceConnector = require('@sliit-foss/service-connector').default;
 
 const connector = serviceConnector({
   baseURL: process.env.BASHAWAY_SERVER_URL,
-  service: 'Bashaway-Server'
+  service: 'Bashaway-Server',
+  headerIntercepts: () => ({
+    'x-api-key': process.env.BASHAWAY_SERVER_API_KEY
+  })
 });
 
 exports.updateScore = (id) => {
