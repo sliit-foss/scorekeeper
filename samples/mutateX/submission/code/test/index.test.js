ded0dcecbc6efba57f04ecbb2c61567536de8bd3
Akalanka <akalankaperera128@gmail.com>
2023-09-02 11:13:30 +0530

    Chore: updated mutateX sample

--- samples/mutateX/submission/code/test/index.test.js
+++ samples/mutateX/submission/code/test/index.test.js
@@ -1,13 +1,10 @@
-const { scan, scanPure, shellFiles } = require('@sliit-foss/bashaway');
-
-let files = [];
-
-beforeAll(() => {
-  files = scan('**', ['src/**']);
-});
+const fs = require('fs');
+const { scan, scanPure, shellFiles, dependencyCount, prohibitedCommands } = require('@sliit-foss/bashaway');
 
 test('should validate if only bash files are present', () => {
-  expect(shellFiles().length).toBe(files.length);
+  const shellFileCount = shellFiles().length;
+  expect(shellFileCount).toBe(1);
+  expect(shellFileCount).toBe(scan('**', ['src/**']).length);
 });
 
 test('should check if there is only one output file', () => {
@@ -15,4 +12,18 @@ test('should check if there is only one output file', () => {
 });
 test('should check if the output file name is mutateX.txt', () => {
   expect(scanPure('./out/**')[0].endsWith('mutateX.txt')).toBe(true);
+});
+describe('should check installed dependencies', () => {
+  let script;
+  beforeAll(() => {
+    script = fs.readFileSync('./execute.sh', 'utf-8');
+  });
+  test('bashaway package functions should not be called', () => {
+    expect(script).not.toContain('@sliit-foss');
+    expect(script).not.toContain('bashaway');
+  });
+  test('no additional npm dependencies should be installed', async () => {
+    await expect(dependencyCount()).resolves.toStrictEqual(3);
+    expect(script).not.toMatch(prohibitedCommands);
+  });
 });
