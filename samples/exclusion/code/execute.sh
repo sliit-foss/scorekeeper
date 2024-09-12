322bdc011ec62655b029da1e4aa48b4d1474e84c
Akalanka Perera <akalankaperera128@gmail.com>
2024-09-12 11:27:08 +0530

    Fix: exclusion submission script

--- samples/exclusion/code/execute.sh
+++ samples/exclusion/code/execute.sh
@@ -1,6 +1,6 @@
 mkdir -p out
 
-find src -type f ! -name '*5334*' | while read file; do
+find src -type f ! -name '*534*' | while read file; do
     filename=$(echo $file | sed 's/src\///g')
     destination=out/$filename
     cp -rf "$file" "$destination"
